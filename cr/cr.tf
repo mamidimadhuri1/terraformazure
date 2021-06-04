b7bf0952da4d7087d7949a774657694196f8e1de
sai charan <saicharan.chunduri@hotmail.com>
2021-06-04 15:12:18 +0000

    Updated cr.tf

--- cr/cr.tf
+++ cr/cr.tf
@@ -9,7 +9,7 @@ terraform {
 
 resource "azurerm_container_registry" "acr" {
   name                     = "devtestenv"
-  resource_group_name      = "temp"
+  resource_group_name      = "cloud-shell-storage-centralindia"
   location                 = "Japan East"
   sku                      = "Standard"
   admin_enabled            = false
