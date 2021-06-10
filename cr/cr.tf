d379b86bdbb807ac3f26d2b3f0776e9b5207e4d2
madhuri mamidi <madhuri.mamidi@cislab.co.in>
2021-06-10 13:42:39 +0000

    Updated cr.tf

--- cr/cr.tf
+++ cr/cr.tf
@@ -9,8 +9,8 @@ terraform {
 
 resource "azurerm_container_registry" "acr" {
   name                     = "devtestenv"
-  resource_group_name      = "temp"
-  location                 = "Japan East"
+  resource_group_name      = "VINCIAZUREDEVOPS01"
+  location                 = "westus2"
   sku                      = "Basic"
   admin_enabled            = false
 }
