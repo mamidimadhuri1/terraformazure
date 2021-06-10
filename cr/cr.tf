730ab8645bbcbbd9fc22e14199d0ca96105e377d
madhuri mamidi <madhuri.mamidi@cislab.co.in>
2021-06-10 13:49:55 +0000

    Updated cr.tf

--- cr/cr.tf
+++ cr/cr.tf
@@ -1,11 +1,4 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "VINCIAZUREDEVOPS01"
-    storage_account_name  = "powershell5567"
-    container_name        = "terraform"
-    key                   = "terraform.tfstate"
-  }
-}
+
 
 resource "azurerm_container_registry" "acr" {
   name                     = "devtestenv"
